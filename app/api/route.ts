7182021b370cb91418ea5e81e53885b9652e6ea0
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-09-08 16:26:39 +0600

    added api base route

---
+++ app/api/route.ts
@@ -0,0 +1,23 @@
+import { authOptions } from '@/utils/authOptions';
+import { getServerSession } from 'next-auth/next';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		return NextResponse.json(
+			{
+				status: 401,
+				success: false,
+				message: 'unauthenticated'
+			},
+			{
+				status: 401
+			}
+		);
+	}
+
+	console.log('GET API', session);
+	return NextResponse.json({ authenticated: !!session });
+}

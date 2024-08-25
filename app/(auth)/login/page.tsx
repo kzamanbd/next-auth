dab4c4680d5b94a99af7aec8ed12dec09617bb91
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-08-25 15:35:12 +0600

    feat: added new login button

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,113 +8,109 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-// export const metadata = {
-// 	title: 'Login'
-// };
-
 export default function Login() {
-	const router = useRouter();
-	const [username, setUsername] = useState('kzamanbn@gmail.com');
-	const [password, setPassword] = useState('password');
-	const [loginError, setLoginError] = useState('');
-	const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+    const [username, setUsername] = useState('kzamanbn@gmail.com');
+    const [password, setPassword] = useState('password');
+    const [loginError, setLoginError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		console.log('login');
-		setIsLoading(true);
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('login');
+        setIsLoading(true);
 
-		const result = await signIn('credentials', {
-			redirect: false,
-			email: username,
-			password: password
-		});
+        const result = await signIn('credentials', {
+            redirect: false,
+            email: username,
+            password: password
+        });
 
-		if (result?.error) {
-			setLoginError(result.error);
-		} else {
-			setLoginError('');
-			router.push('/dashboard');
-		}
+        if (result?.error) {
+            setLoginError(result.error);
+        } else {
+            setLoginError('');
+            router.push('/dashboard');
+        }
 
-		console.log(result);
-		setIsLoading(false);
-	};
+        console.log(result);
+        setIsLoading(false);
+    };
 
-	return (
-		<div className="bg-light-gray dark:bg-dark-secondary dark:text-gray-300 lg:h-screen lg:overflow-hidden">
-			<div className="flex min-h-screen items-center justify-center p-6">
-				<div className="login-bg">
-					<div className="rounded-md bg-white p-8 shadow-md">
-						<div className="my-4 flex items-center justify-center space-x-2">
-							<span className="h-12 w-12">
-								<ApplicationLogo />
-							</span>
-							<span className="dark--text text-3xl font-semibold">Next Auth</span>
-						</div>
-						<p className="text-xs text-center text-gray-600">
-							Please sign-in to your account and start the adventure
-						</p>
+    return (
+        <div className="bg-light-gray dark:bg-dark-secondary dark:text-gray-300 lg:h-screen lg:overflow-hidden">
+            <div className="flex min-h-screen items-center justify-center p-6">
+                <div className="login-bg">
+                    <div className="rounded-md bg-white p-8 shadow-md">
+                        <div className="my-4 flex items-center justify-center space-x-2">
+                            <span className="h-12 w-12">
+                                <ApplicationLogo />
+                            </span>
+                            <span className="dark--text text-3xl font-semibold">Next Auth</span>
+                        </div>
+                        <p className="text-xs text-center text-gray-600">
+                            Please sign-in to your account and start the adventure
+                        </p>
 
-						<OtherLoginOption />
+                        <OtherLoginOption />
 
-						<form className="mt-4" onSubmit={handleSubmit}>
-							{loginError && <div className="text-red-500 text-center">{loginError}</div>}
-							<label className="block">
-								<span className="form-label">Email</span>
-								<input
-									type="email"
-									name="email"
-									className="form-input"
-									placeholder="Email Address"
-									value={username}
-									required
-									onChange={(e) => setUsername(e.target.value)}
-								/>
-							</label>
+                        <form className="mt-4" onSubmit={handleSubmit}>
+                            {loginError && <div className="text-red-500 text-center">{loginError}</div>}
+                            <label className="block">
+                                <span className="form-label">Email</span>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    className="form-input"
+                                    placeholder="Email Address"
+                                    value={username}
+                                    required
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </label>
 
-							<label className="mt-3 block">
-								<span className="form-label">Password</span>
-								<input
-									type="password"
-									name="password"
-									className="form-input"
-									placeholder="********"
-									value={password}
-									required
-									onChange={(e) => setPassword(e.target.value)}
-								/>
-							</label>
+                            <label className="mt-3 block">
+                                <span className="form-label">Password</span>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="form-input"
+                                    placeholder="********"
+                                    value={password}
+                                    required
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </label>
 
-							<div className="mt-4 flex items-center justify-between">
-								<label className="inline-flex items-center">
-									<input type="checkbox" name="remember" className="form-checkbox" />
-									<span className="dark--text mx-2 text-sm">Remember me</span>
-								</label>
+                            <div className="mt-4 flex items-center justify-between">
+                                <label className="inline-flex items-center">
+                                    <input type="checkbox" name="remember" className="form-checkbox" />
+                                    <span className="dark--text mx-2 text-sm">Remember me</span>
+                                </label>
 
-								<a
-									className="block text-sm text-primary-600 hover:underline"
-									href="/auth-forgot-password.html">
-									Forgot your password?
-								</a>
-							</div>
+                                <a
+                                    className="block text-sm text-primary-600 hover:underline"
+                                    href="/auth-forgot-password.html">
+                                    Forgot your password?
+                                </a>
+                            </div>
 
-							<div className="mt-6">
-								<button type="submit" className="btn btn-primary flex w-full" disabled={isLoading}>
-									<span className="mr-2">Sign in to your account</span>
-									<LoadingSpinner isLoading={isLoading} />
-								</button>
-							</div>
-						</form>
-						<p className="dark--text mt-4">
-							Don’t have an account yet?
-							<Link href="/register" className="text-primary ml-2">
-								Sign up here
-							</Link>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                            <div className="mt-6">
+                                <button type="submit" className="btn btn-primary flex w-full" disabled={isLoading}>
+                                    <span className="mr-2">Sign in to your account</span>
+                                    <LoadingSpinner isLoading={isLoading} />
+                                </button>
+                            </div>
+                        </form>
+                        <p className="dark--text mt-4">
+                            Don’t have an account yet?
+                            <Link href="/register" className="text-primary ml-2">
+                                Sign up here
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }

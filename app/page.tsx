db49926c6f8430ce0052c13bae6fe8384679deee
KAMRUZZAMAN <kzamanbn@gmail.com>
2025-01-20 16:36:56 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,10 @@ const Home = async () => {
     }
     return (
         <div className="h-screen flex items-center justify-center">
-            <Link
-                href="/login"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            <Link href="/login" className="btn btn-primary">
                 Login
             </Link>
-            <Link
-                href="/register"
-                className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+            <Link href="/register" className="btn btn-light">
                 Register
             </Link>
         </div>
